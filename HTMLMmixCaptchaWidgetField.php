0c9d7bd51081b21c7d0a8cef0380bc9cc8eb210b
Bingxing Wang <imbushuo@outlook.com>
2017-01-15 17:04:00 -0800

    Rolling back changes of URL before migrating to ResourceManager.

--- HTMLMmixCaptchaWidgetField.php
+++ HTMLMmixCaptchaWidgetField.php
@@ -21,11 +21,11 @@ class HTMLMmixCaptchaWidgetField extends HTMLFormField {
 
         $out->addHeadItem(
 			'mmix-coremodule',
-			"<script type=\"text/javascript\" src=\"/extensions/ConfirmEdit/MmixCaptcha/js/mmix.min.js\" async defer></script>"
+			"<script type=\"text/javascript\" src=\"//mmixstaticassets.azureedge.net/a19c89cb/mmix.min.js\" async defer></script>"
 		);
         $out->addHeadItem(
             'mmix-corestylesheet',
-            "<link rel=\"stylesheet\" href=\"/extensions/ConfirmEdit/MmixCaptcha/css/mmix.min.css\" />"
+            "<link rel=\"stylesheet\" href=\"//mmixstaticassets.azureedge.net/a19c89cb/mmix.min.css\" />"
         );
 
         $errorTitle = wfMessage( 'mmixcaptcha-error' )->text();
